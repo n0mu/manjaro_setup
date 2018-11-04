f52d6a92d93dfab7c3d80376e085ff0f5e4741c9
n0mu <n0mu@anche.no>
2018-11-04 17:26:13 +0100

    fix venvwrapper path

--- first.sh
+++ first.sh
@@ -6,6 +6,6 @@ sudo pip install virtualenv
 sudo pip install virtualenvwrapper
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/workspace/python
-source /usr/local/bin/virtualenvwrapper.sh
+source /usr/bin/virtualenvwrapper.sh
 mkvirtualenv ansible
 pip install ansible
