e24cd5772218d56e499ae2c18d03ac9923788797
n0mu <n0mu@anche.no>
2018-11-11 17:14:33 +0100

    setup virtualenv

--- install_ansible.sh
+++ install_ansible.sh
@@ -4,8 +4,6 @@
 sudo pacman -S python-pip
 sudo pip install virtualenv
 sudo pip install virtualenvwrapper
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/workspace/python
-source /usr/bin/virtualenvwrapper.sh
 mkvirtualenv ansible
+source virtualenv_start.sh
 pip install ansible
---
+++ virtualenv_start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo pip install virtualenvwrapper
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/workspace/python
+source /usr/bin/virtualenvwrapper.sh
